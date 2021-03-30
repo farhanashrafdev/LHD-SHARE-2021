15c5013fdb7fdd33da48533e934c4837c5cbd417
Farhan Ashraf <farhanashrafali30@gmail.com>
2021-03-30 00:29:36 -0700

    Delete Form-made using Flask-python directory

--- Form-made using Flask-python/app.py
+++
@@ -1,8 +0,0 @@
-from flask import Flask, render_template
-
-app = Flask(__name__)
-
-@app.route('/subscribe')
-def subscribe():
-    title = "Subscribe to the newsletter"
-    return render_template("sub.html", title=title)
